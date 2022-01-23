98b9e360d4766fdd5895da4bd231a3723adac199
yujin <dksdbwls15@naver.com>
2022-01-23 20:04:46 +0900

    while

---
+++ 2장/js/text-color.js
@@ -0,0 +1,7 @@
+ //heading변수 선언 후 h1태그를 저장함 
+ var heading =document.querySelector("#heading");
+        
+ //h1태그를 클릭 하면 글자 색깔이 red로 바뀜 
+ heading.onclick =function() {
+     heading.style.color ="red";
+ }
