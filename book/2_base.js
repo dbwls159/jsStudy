ed295928ba7be766f7ce9a9390953d0dca981d13
yujin <dksdbwls15@naver.com>
2022-02-08 17:11:28 +0900

    door

---
+++ book/2_base.js
@@ -0,0 +1,22 @@
+// 키워드 : 수행할 동작 규정 ex) var 키워드는 새로운 변수 생성 지시 
+
+var x= 5+6; //변수 선언
+
+function foo (arg) { //함수 선언
+
+    return ++ arg; //함수종료 및 값의 반환
+}
+
+var i = 0 ;
+
+//반복문 
+while (1) {
+
+    if(i>5){
+        //반복문 탈출
+        break;
+    
+    }
+    console.log(i);
+    i++;
+}
