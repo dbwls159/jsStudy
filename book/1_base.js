e1e7ffb032d64a2bc54b091a19529604e845fd9e
yujin <dksdbwls15@naver.com>
2022-02-08 19:06:27 +0900

    삭제 후 옮김

--- book/1_base.js
+++ book/1_base.js
@@ -9,7 +9,37 @@
 var x ; // 변수 선언
 x = 6 ; //정수 값 할당 
 
-console.log(x);
 
+var str = 'hello world' // 변수 선언, 문자열 리터럴 값 할당
 
+// js는 동적으로 변수의 타입이 결정된다.
+// js의 모든 값은 데이터 타입을 갖는다. 7가지 타입 있음
+// 원시타입(number,string,boolean,null,undefined,symbol)
+//객체타입(object)
 
+var num1 = 1001; 
+var num2 = 10.52;
+
+var string1='hello';
+var string2="World";
+
+var bool = true;
+
+var foo= null;
+
+var obj={name:'Lee',gender:'male'};
+
+var array=[1,2,3];
+
+var foo=function() {};
+
+//연산자는 하나이상의 표현식을 대상으로 산술,할당,비교,논리,타입 연산등을 수행해 하나의 값을 만듬
+// 연산의 대상을 피연산자라 한다.
+
+var area=5*4;
+var str='My name is' +'An'; //문자열 연결
+var color = 'red' //할당 연산자
+var foo = 3>5; //비교
+var bar =(5>3) && (2<4); //논리
+var type= typeof 'Hi';
+var today =new Data(); //인스턴스 생성 연산자
--- book/2_base.js
+++ book/2_base.js
@@ -15,8 +15,22 @@ while (1) {
     if(i>5){
         //반복문 탈출
         break;
-    
     }
     console.log(i);
     i++;
+}
+
+//문 : 리터럴, 연산자, 표현식, 키워드 등 구성, ; 으로 끝나야함
+//문은 코드 블록 {...} 그룹화 할수 있음.함께 실행되어야하는 문을 정의하기 위함.
+// 조건문,반복문의 사용으로 제어 = 흐름 제어 
+function myFunction(x,y){
+    return x+y;
+}
+
+if (x>0){
+    //something
+}
+
+for (var i =0; i<10; i++){
+    //something
 }
